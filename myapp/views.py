5b37e94b574df5e63618a6aba4d882d790e71ab1
Alex.Dai <re757575@gmail.com>
2015-12-01 18:12:13 +0800

    Added & Modify: use template

--- myapp/views.py
+++ myapp/views.py
@@ -1,5 +1,5 @@
 from django.http import HttpResponse
-
+from django.shortcuts import render
 import random
 
 def home(request):
@@ -10,24 +10,5 @@ def home(request):
         "It works on my machine",
     ]
 
-    output = """
-<!DOCTYPE HTML>
-<html style="height: 100%">
-<head>
- <title>Clone of Excuses For Lazy Coders</title>
-</head>
-<body style="height: 100%">
- <div class="wrapper" style="min-height: 100%; height: auto !important; height: 100%; margin: 0 auto -8em;">
-  <center style="color: #333; padding-top: 200px; font-family: Courier; font-size: 24px; font-weight: bold;"><a href="/" rel="nofollow" style="text-decoration: none; color: #333;">{excuse}</a></center>
-  <div class="push"></div>
- </div>
- <div class="footer">
-  <center style="color: #333; font-family: Courier; font-size: 11px;">
-   <br /><br />&copy; Copyleft
-  </center>
- </div>
- </body>
-</html>
-    """.format(excuse=random.choice(excuses))
-
-    return HttpResponse(output)
+    excuse = random.choice(excuses)
+    return render(request, "index.html", {'excuse': excuse})
