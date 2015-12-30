9d95ebec394f80eccefb7215de6efdd47b46c38f
Alex.Dai <re757575@gmail.com>
2015-12-30 11:31:49 +0800

    Modify: 修改 manage.py test 無法執行的問題.. TestCase 需使用db , SimpleTestCase 則需不要

--- app/tests.py
+++ app/tests.py
@@ -5,13 +5,21 @@ when you run "manage.py test".
 Replace this with more appropriate tests for your application.
 """
 
+# Integration test (with db)
 from django.test import TestCase
 
+# Unit test (without db)
+from django.test import SimpleTestCase
 
-class SimpleTest(TestCase):
-
+class SimpleTest(SimpleTestCase):
     def test_basic_addition(self):
         """
         Tests that 1 + 1 always equals 2.
         """
         self.assertEqual(1 + 1, 2)
+
+    def test_basic_addition2(self):
+        """
+        Tests that 2 * 2 always equals 4.
+        """
+        self.assertEqual(2 * 2, 4)
